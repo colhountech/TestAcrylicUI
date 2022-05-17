c5aa2bfb7fd19e423e09f0223561281cf866cb3f
Micheal Colhoun <micheal@cts-ni.com>
2022-05-17 19:33:47 +0100

    Add project files.

---
+++ App1AcrylicUI/Form1.cs
@@ -0,0 +1,27 @@
+﻿using AcrylicUI.Forms;
+using AcrylicUI.Resources;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace App1AcrylicUI
+{
+    public partial class Form1 : AcrylicForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.Text = "";
+            this.IsAcrylic = true;
+            this.BlurColor = Colors.GreyBackground; // This keeps getting overwritten when you change Designer properties
+
+        }
+
+    }
+}
---
+++ App2AcrylicUI/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace App2AcrylicUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.windowPanel1 = new AcrylicUI.Controls.WindowPanel();
+            this.acrylicButton1 = new AcrylicUI.Controls.AcrylicButton();
+            this.acrylicButton2 = new AcrylicUI.Controls.AcrylicButton();
+            this.acrylicButton3 = new AcrylicUI.Controls.AcrylicButton();
+            this.acrylicButton4 = new AcrylicUI.Controls.AcrylicButton();
+            this.acrylicButton5 = new AcrylicUI.Controls.AcrylicButton();
+            this.acrylicButton6 = new AcrylicUI.Controls.AcrylicButton();
+            this.windowPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // windowPanel1
+            // 
+            this.windowPanel1.Controls.Add(this.acrylicButton6);
+            this.windowPanel1.Controls.Add(this.acrylicButton5);
+            this.windowPanel1.Controls.Add(this.acrylicButton4);
+            this.windowPanel1.Controls.Add(this.acrylicButton3);
+            this.windowPanel1.Controls.Add(this.acrylicButton2);
+            this.windowPanel1.Controls.Add(this.acrylicButton1);
+            this.windowPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.windowPanel1.Icon = null;
+            this.windowPanel1.IsAcrylicEnabled = false;
+            this.windowPanel1.Location = new System.Drawing.Point(0, 0);
+            this.windowPanel1.Name = "windowPanel1";
+            this.windowPanel1.ProfileFeature = false;
+            this.windowPanel1.RoundCorners = false;
+            this.windowPanel1.SectionHeader = null;
+            this.windowPanel1.Size = new System.Drawing.Size(800, 450);
+            this.windowPanel1.TabIndex = 0;
+            // 
+            // acrylicButton1
+            // 
+            this.acrylicButton1.Default = false;
+            this.acrylicButton1.ImagePadding = 6;
+            this.acrylicButton1.Location = new System.Drawing.Point(40, 52);
+            this.acrylicButton1.Name = "acrylicButton1";
+            this.acrylicButton1.Padding = new System.Windows.Forms.Padding(6);
+            this.acrylicButton1.Size = new System.Drawing.Size(160, 32);
+            this.acrylicButton1.TabIndex = 0;
+            this.acrylicButton1.Text = "acrylicButton1";
+            this.acrylicButton1.UseVisualStyleBackColor = false;
+            // 
+            // acrylicButton2
+            // 
+            this.acrylicButton2.Default = false;
+            this.acrylicButton2.ImagePadding = 6;
+            this.acrylicButton2.Location = new System.Drawing.Point(40, 105);
+            this.acrylicButton2.Name = "acrylicButton2";
+            this.acrylicButton2.Padding = new System.Windows.Forms.Padding(6);
+            this.acrylicButton2.Size = new System.Drawing.Size(160, 32);
+            this.acrylicButton2.TabIndex = 1;
+            this.acrylicButton2.Text = "acrylicButton2";
+            this.acrylicButton2.UseVisualStyleBackColor = false;
+            // 
+            // acrylicButton3
+            // 
+            this.acrylicButton3.Default = false;
+            this.acrylicButton3.ImagePadding = 6;
+            this.acrylicButton3.Location = new System.Drawing.Point(40, 158);
+            this.acrylicButton3.Name = "acrylicButton3";
+            this.acrylicButton3.Padding = new System.Windows.Forms.Padding(6);
+            this.acrylicButton3.Size = new System.Drawing.Size(160, 32);
+            this.acrylicButton3.TabIndex = 2;
+            this.acrylicButton3.Text = "acrylicButton3";
+            this.acrylicButton3.UseVisualStyleBackColor = false;
+            // 
+            // acrylicButton4
+            // 
+            this.acrylicButton4.Default = false;
+            this.acrylicButton4.ImagePadding = 6;
+            this.acrylicButton4.Location = new System.Drawing.Point(40, 211);
+            this.acrylicButton4.Name = "acrylicButton4";
+            this.acrylicButton4.Padding = new System.Windows.Forms.Padding(6);
+            this.acrylicButton4.Size = new System.Drawing.Size(160, 32);
+            this.acrylicButton4.TabIndex = 3;
+            this.acrylicButton4.Text = "acrylicButton4";
+            this.acrylicButton4.UseVisualStyleBackColor = false;
+            // 
+            // acrylicButton5
+            // 
+            this.acrylicButton5.Default = false;
+            this.acrylicButton5.ImagePadding = 6;
+            this.acrylicButton5.Location = new System.Drawing.Point(40, 264);
+            this.acrylicButton5.Name = "acrylicButton5";
+            this.acrylicButton5.Padding = new System.Windows.Forms.Padding(6);
+            this.acrylicButton5.Size = new System.Drawing.Size(160, 32);
+            this.acrylicButton5.TabIndex = 4;
+            this.acrylicButton5.Text = "acrylicButton5";
+            this.acrylicButton5.UseVisualStyleBackColor = false;
+            // 
+            // acrylicButton6
+            // 
+            this.acrylicButton6.Default = false;
+            this.acrylicButton6.ImagePadding = 6;
+            this.acrylicButton6.Location = new System.Drawing.Point(40, 317);
+            this.acrylicButton6.Name = "acrylicButton6";
+            this.acrylicButton6.Padding = new System.Windows.Forms.Padding(6);
+            this.acrylicButton6.Size = new System.Drawing.Size(160, 32);
+            this.acrylicButton6.TabIndex = 5;
+            this.acrylicButton6.Text = "acrylicButton6";
+            this.acrylicButton6.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BlurColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.windowPanel1);
+            this.Location = new System.Drawing.Point(0, 0);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.windowPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private AcrylicUI.Controls.WindowPanel windowPanel1;
+        private AcrylicUI.Controls.AcrylicButton acrylicButton6;
+        private AcrylicUI.Controls.AcrylicButton acrylicButton5;
+        private AcrylicUI.Controls.AcrylicButton acrylicButton4;
+        private AcrylicUI.Controls.AcrylicButton acrylicButton3;
+        private AcrylicUI.Controls.AcrylicButton acrylicButton2;
+        private AcrylicUI.Controls.AcrylicButton acrylicButton1;
+    }
+}
